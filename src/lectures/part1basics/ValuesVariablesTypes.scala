52e0b43072b2f2c6e4dcfa6f41f383d3a666e61b
Ian King <ian.king@clearscore.com>
2022-05-12 16:20:10 +0100

    Boolean type

--- src/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -1,11 +1,12 @@
 package lectures.part1basics
 
 object ValuesVariablesTypes extends App {
+
   val x: Int = 42  // Explicitly stating that 42 is an Int, its not inferred by the compiler.
   println(x)
-
   // VALS ARE IMMUTABLE - works similar to constants but not regarded as constants to be re-used throughout the code.
   // More as an intermediate computations to later use in bigger computations.
+
 
 
 
@@ -14,8 +15,19 @@ object ValuesVariablesTypes extends App {
 
 
 
+
   val aString: String = "hello this is a string"; // Scala doesnt require semi colons.
   val anotherString: String = "goodbye"
+  println(aString)
+  println(anotherString)
+
+
+
+  val aBoolean: Boolean = true
+  val anotherBoolean: Boolean = false
+  println(aBoolean)
+  println(anotherBoolean)
+
 
 
 
