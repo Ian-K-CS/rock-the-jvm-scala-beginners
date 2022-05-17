f4ded031dc73d6acd5cbfb6459507cd118b52a68
Ian King <ian.king@clearscore.com>
2022-05-17 15:01:23 +0100

    relational & logical operators

--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -7,8 +7,7 @@ object Expressions extends App {
 
   println(2 + 4 * 5 + (3 - 4)) // A math expression. Scala preserves math operator precedence (BIDMAS)
 
-  // Arithmetic Operators
-
+  // ARITHMETIC OPERATORS
   val a: Int = 10
   val b: Int = 3
   println("--Arithmetic Operators--")
@@ -18,4 +17,23 @@ object Expressions extends App {
   println(a / b) // Division
   println(a % b) // Modulo
 
+  // RELATIONAL OPERATORS
+  println("--Relational Operators--")
+  println(1 == "x") // Equal
+  println(1 != "x") // Not equal
+  println(5 > 1) // Greater than
+  println(5 < 1) // Less than
+  println(5 >= 5) // Greater or equal to
+  println(3 <= 5) // Less than or equal to
+
+  // LOGICAL OPERATORS
+  println("--Logical Operators--")
+  println(!true) // Not - reverses logic state
+  println(false && false) // AND - returns true if both operands are Boolean true
+  println(true || false) // OR returns true of one operand is Boolean true
+
 }
+
+
+// Questions
+// What in scala is considered truthy / falsy?
