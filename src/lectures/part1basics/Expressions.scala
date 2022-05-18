cd300ebaab86c9e64933ecd692c05388fd228c87
Ian King <ian.king@clearscore.com>
2022-05-18 09:25:04 +0100

    more examples of string manipulation and multiline strings

--- src/lectures/part1basics/Expressions.scala
+++ src/lectures/part1basics/Expressions.scala
@@ -55,23 +55,35 @@ object Expressions extends App {
 
 
 
-  // String interpolation examples
-  val firstName = "John"
-  val middleName = "S."
-  val lastName =  "Smith"
+  // STRING INTERPOLATION EXAMPLES
+
+  //println("blah blah blah") will always end in a new line '\n'
+  //print("blah blah") will always print does not add a new line
+
+  val firstName: String = "John"
+  val middleName: String = "S."
+  val lastName: String = "Smith"
 
   val fullName = s"$firstName $middleName $lastName" // Can string interpolate this way.
   println(fullName)
-
-  println(s"${firstName} ${middleName} ${lastName}") // Or like this with curly brackets - like Ruby
-
+  println(s"${firstName} ${middleName} ${lastName}") // Or like this with curly brackets - can also be used to evaluate expressions - like Ruby
   println(firstName + " " + middleName + " " + lastName) // Or append the Strings together - like Ruby
 
+  print("line 1")
+  print("line 2")
+  print("line 3" + "\n") // This will print but won't include a new line - has to be added manually
 
+  // Multiline string example - requires """ blah blah blah """"
+  val speach: String =
+    """Four score and
+      |seven years ago
+      |our fathers""".stripMargin
+  print(speach + "\n")
+  println(speach)
 }
 
 
 // Questions
 // check line 42 - why doesn't this work ?
-//what is 'run' in intellij - is it a terminal? Ruby just printed lines to the terminal directly
+// what is 'run' in intellij - is it a terminal? Ruby just printed lines to the terminal directly
 // What in scala is considered truthy / falsy? - does scala even have this?
