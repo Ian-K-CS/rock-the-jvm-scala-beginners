2907f287cea540582539ed065ebc003933eeebb7
Ian King <ian.king@clearscore.com>
2022-05-24 14:16:59 +0100

    recursion initial method

---
+++ src/lectures/part1basics/Recursion.scala
@@ -0,0 +1,17 @@
+package lectures.part1basics
+
+object Recursion extends App {
+
+  def factorial(n: Int): Int = {
+    if (n <= 1) 1
+    else {
+      println("computing factorial of " + n + " - I first need factorial of " + (n - 1))
+      val result = n * factorial(n - 1)
+      println("computed factorial of " + n)
+      result
+    }
+  }
+
+  println(factorial(5))
+
+}
