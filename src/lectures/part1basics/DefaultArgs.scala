a856bbcafedfe184450fd2a03c3251807eae069c
Ian King <ian.king@clearscore.com>
2022-05-26 14:41:10 +0100

    finished default values for parameters

--- src/lectures/part1basics/DefaultArgs.scala
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -2,6 +2,8 @@ package lectures.part1basics
 
 object DefaultArgs extends App {
 
+  // when you call a method a lot, and the parameter doesn't change a lot .... maybe add in a default value instead!
+
   def trFact(n: Int, acc: Int = 50): Int = { // can assign a default value for an argument... in this example, 'acc' is an Int with a default value of 50
     if (n <= 1) acc
     else {
@@ -15,4 +17,9 @@ object DefaultArgs extends App {
 
   println(fact10)
 
+
+
+  def savePicture(format: String = "jpg", width: Int, height: Int): Unit = println("saving picture")
+
+  savePicture(width = 800, height = 500)
 }
