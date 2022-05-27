59bca5d423cffd2eb3cda63d0f614d5dd09f906e
Ian King <ian.king@clearscore.com>
2022-05-27 15:23:07 +0100

    OOP basics in scala - complete

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -2,17 +2,47 @@ package lectures.part2oop
 
 object OOBasics extends App {
 
-  val person = new Person(name = "Dave", age = 26) // instantiating a new object of the Person class. 'age' and 'name are class parameter, but aren't class member, so you can't access it
 
-  println(person.name)
+  // instantiating a new object of the Person class. 'age' and 'name are class parameter, but aren't class member, so you can't access it
+  // at instantiation of a new Person object, the code within the Class code block will run!
+  val person = new Person(name = "Dave", age = 26)
+
   println(person.age)
+  person.greet(name = "Reginald")
+  person.greet()
 
 }
 
-class Person(val name: String, val age: Int) // Class constructor. Basically says every instance of Person must be constructed by passing in (in this case) a 'name' and 'age'.
-                                             // This also initialises the object with setting the instance variable / field values.. 'val name: String' & 'val age: Int'.
 
 
+// Class constructor. Basically says every instance of Person must be constructed by passing in (in this case) a 'name' and 'age'.
+// This also initialises the object with setting the instance variable / field values.. 'val age: Int'. You don't HAVE to set each parameter as a field - I haven't with 'name: String'
+class Person(name: String, val age: Int) {
+  // class body - everything between '{}' defines the implementation of the class.
+
+
+  // Any var's / val's defined in a class are also fields / instance variables of said Class.
+  val x: Int = 2
+
+
+  // at instantiation of a new Person object, the entire code within the Class code block will run. Which is why this prints before lines 7 & 8.
+  println(1 + 3)
+
+
+  // methods defined in a class are called methods. Ie, it can be called on an object. Functions aren't associated with a class and aren't called on objects.
+  // 'this' relates to the current objects instance variable/field/class parameter. It gives you access to the objects instance variables / fields / class parameters.. state
+  def greet(name: String): Unit = println(s"Welcome ${this.name} - $name says hello!")
+
+
+  // overloading - defining methods with the same name but different method signatures
+  def greet(): Unit = println(s"$name")
+
+
+  // Auxiliary Constructors 'this(name: String)' that calls the Primary constructor 'this(name, 0)' - I don't this concept is used to much!
+  def this(name: String) = this(name, 0)
+
+
+}
 
 
 
@@ -26,3 +56,4 @@ class Person(val name: String, val age: Int) // Class constructor. Basically say
 // Auxiliary Constructor - other constructors other than primary constructor. Can has many auxiliary constructors.
 
 // Initialisation - assignment of initial value(s) for instance variables / fields.
+
