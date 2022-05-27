4f0cf8a9f3230e173dc8cf8b2f1d56d5b1beeb6c
Ian King <ian.king@clearscore.com>
2022-05-27 13:55:14 +0100

    OO Basics in Scala

---
+++ src/lectures/part2oop/OOBasics.scala
@@ -0,0 +1,9 @@
+package lectures.part2oop
+
+object OOBasics extends App {
+
+  val person = new Person // instantiating a new object of the Person class.
+  println(person)
+}
+
+class Person // defining our class
